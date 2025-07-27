c61602bb2b13135da89cf5d538baeb19c2f13c8f
Selorm Ahorlu <selormonray14@gmail.com>
2025-07-27 16:29:41 +0000

    updated the HomePage test

--- src/main/java/org/example/HomePage.java
+++ src/main/java/org/example/HomePage.java
@@ -4,39 +4,67 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class HomePage extends PageObject{
+import static org.example.BrowserFactory.BASE_URL;
 
-    public HomePage (WebDriver driver) {
+public class HomePage extends PageObject {
+    private final By createEmailInput = By.id("email_create");
+    private final By createAccountButton = By.id("SubmitCreate");
+    private final By signInEmailInput = By.id("email");
+    private final By passwordInput = By.id("passwd");
+    private final By signInButton = By.id("SubmitLogin");
+
+    public HomePage(WebDriver driver) {
         this.driver = driver;
     }
 
-    private final By createEmailInput = By.id("email_create");
-    private final By createAccountButton = By.id("SubmitCreate");
-
-    public HomePage openHomepage() {
-        driver.get("http://www.automationpractice.pl/index.php?controller=authentication&back=my-account#account-creation");
+    // Sign Up Methods
+    public HomePage enterEmail(String email) {
+        getWebElement(createEmailInput).sendKeys(email);
         return this;
     }
 
-    //        enter first name
-    private WebElement getCreateEmailInput() {
-        return getWebElement(createEmailInput);
-    }
-    public HomePage enterEmail(final String email) {
-        getCreateEmailInput().sendKeys(email);
-        return this;
-    }
-
-
-
-    private WebElement getCreateAccountButton() {
-        return getWebElement(createAccountButton);
-    }
     public HomePage clickCreateAccountBtn() {
-       getCreateAccountButton().click();
-       return this;
+        getWebElement(createAccountButton).click();
+        return this;
     }
 
+    // Sign In Methods
+    public HomePage openSignInPage() {
+        driver.get("http://www.automationpractice.pl/index.php?controller=my-account");
+        return this;
+    }
 
+    public HomePage openHomePage() {
+        driver.get(BrowserFactory.BASE_URL);
+        return this;
+    }
 
+    public HomePage enterSignInEmail(String email) {
+        getWebElement(signInEmailInput).sendKeys(email);
+        return this;
+    }
+
+    public HomePage enterSignInPassword(String password) {
+        getWebElement(passwordInput).sendKeys(password);
+        return this;
+    }
+
+    public HomePage clickSignInButton() {
+        getWebElement(signInButton).click();
+        return this;
+    }
+
+    // Combined Methods for Better Flow
+    public HomePage createNewAccount(String email) {
+        enterEmail(email);
+        clickCreateAccountBtn();
+        return this;
+    }
+
+    public HomePage signIn(String email, String password) {
+        enterSignInEmail(email);
+        enterSignInPassword(password);
+        clickSignInButton();
+        return this;
+    }
 }
