91c4f30a7ca1cbcbf68340f8152eb96366a7a63e
Selorm Ahorlu <selormonray14@gmail.com>
2025-08-10 13:07:09 +0000

    updating the test class

--- src/test/java/org/example/SignUpTest.java
+++ src/test/java/org/example/SignUpTest.java
@@ -4,6 +4,7 @@ import Enum.UserDOB;
 import Enum.UserNames;
 import com.github.javafaker.Faker;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -11,42 +12,22 @@ import org.testng.annotations.Test;
 
 public class SignUpTest {
     private WebDriver driver;
+    private WebDriverWait wait;
+    private HomePage homePage;
+    private SignUpPage signUpPage;
+    private String userEmail;
 
     @BeforeMethod
     public void setUp() {
         driver = BrowserFactory.getDriver("chrome");
+        wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(10));
+        homePage = new HomePage(driver);
+        signUpPage = new SignUpPage(driver);
+        userEmail = new Faker().internet().emailAddress();
     }
 
     @Test
-    void createAccount() throws InterruptedException {
-        HomePage homePage = new HomePage(driver);
-        Faker faker = new Faker();
-        homePage.openHomePage();
 
-        homePage.enterEmail(faker.internet().emailAddress());
-        homePage.clickCreateAccountBtn();
-
-        SignUpPage signUpPage = new SignUpPage(driver);
-        Thread.sleep(5000L);
-
-        signUpPage.enterFirstName(UserNames.TOM.getDisplayText());
-        signUpPage.enterLastName(UserNames.SMITH.getDisplayText());
-        signUpPage.enterPassword(UserNames.PASSWORD.getDisplayText());
-        signUpPage.selectDaysFromDropdown(UserDOB.DAY_OF_BIRTH.getDropdownValue());
-        signUpPage.selectMonthsFromDropdown(UserDOB.MONTH_OF_BIRTH.getDropdownValue());
-        signUpPage.selectYearsFromDropdown(UserDOB.YEAR_OF_BIRTH.getDropdownValue());
-
-        verifyDetailsInSignUpForm();
-        signUpPage.clickRegisterBtn();
-        Thread.sleep(5000L);
-    }
-
-    private void verifyDetailsInSignUpForm() {
-        SignUpPage signUpPage = new SignUpPage(driver);
-        Assert.assertEquals(signUpPage.getFirstNameFromInputField(), UserNames.TOM.getDisplayText());
-        Assert.assertEquals(signUpPage.getLastNameFromInputField(), UserNames.SMITH.getDisplayText());
-        Assert.assertEquals(signUpPage.getPasswordFromInputField(), UserNames.PASSWORD.getDisplayText());
-    }
 
     @AfterMethod
     public void tearDown() {
